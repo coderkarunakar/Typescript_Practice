b859f70c702b7f0c6f8bf36fe183068bff07dc8a
karunakar <chembetikarunakar@gmail.com>
2026-02-05 08:34:56 +0530

    INfor wont work for typeof and Jsonparse

--- index.js
+++ index.js
@@ -3,35 +3,19 @@
 //string - text type
 //bigint -whole numbers and floating point values but allows larger negative and postive nubmers than the number type
 //symbol - it is used to create globally unique identifier
-
-
-
 //there are 2 types of variable declaration in ts
 //1Explict -where we manually declare the variable data type
 //Example
-let firstName : String = "karunakar"
+var firstName = "karunakar";
 //in the above it is similar to Js declaration simply we just mentioned its data type as string which is explicit
 // 2.Implicity
-let lastName = "Chembeti"
+var lastName = "Chembeti";
 //here we didn't mentioned its datatype by defult it will read as string this is called implicit
-
 //here we can modify the declared variables but only limited to declared datatype here datatype is string so only string is possible but not the number
-firstName = "hello"
-
+firstName = "hello";
 // firstName = 3 (here i tried to declare as a number whose datatype is string which gives error)
-
-
 //Note: this automatic identifing the value is knows as  "INFOR" in TypeScript
-
-
-
-
-
-var json  = JSON.parse("55");//json.parse always return the actual datatype
-console.log(typeof json);//output will be number
-
-var json1:string  = JSON.parse("55");//json.parse always return the actual datatype
-console.log(typeof json1);//output will be nubmer 
-
-
-
+var json = JSON.parse("55"); //json.parse always return the actual datatype
+console.log(typeof json); //output will be number
+var json1 = JSON.parse("55"); //json.parse always return the actual datatype
+console.log(typeof json1); //output will be nubmer 
