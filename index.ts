e05cb28c71eecdb8f9712df86a5a167775c00292
karunakar <chembetikarunakar@gmail.com>
2026-02-14 15:31:33 +0530

    type alias

--- index.ts
+++ index.ts
@@ -1,48 +1,21 @@
-//Enum
-//enum is used to define the string or number a group of named constants
-//syntax
+//Type alias, and type interface
+type UserName = string;
+let name: UserName = "Karun";
+//it has 3 parts here name is the variable ,userName is the type alias,  karun is the value,here username defines which type should we declare and name is the actual storage holding value
 
-enum cardinalDirections {
-    North,
-    South,
-    East,
-    West
+
+
+
+//object
+type User = {
+    id:number;
+    name:string,
+    isAdmin:boolean
 }
-let currentDirection = cardinalDirections.East
-console.log(currentDirection);
-
-//Note: in typescript we can define its index number as well
-
-enum cardinalDirections1 {
-    North =2,
-    South,
-    East,
-    West = 7
-}
-let currentDirection1 = cardinalDirections1.West
-console.log(currentDirection1)
-
-//see here above we have modified its index value by defining its index start value this is the beauty of typescript in enums
+const holder: User = {id:1,name:'karun',isAdmin:true};
 
 
-//in enum we can give only string or number
-enum statusCode  {
-    NotFound = "karun",
-    Success = 200,
-    Accepted = 202,
-    BadRequest = "400"
-}
+//funtion
+type Add = (a: number, b:number) =>number;
+const sum :Add = (a,b) => a+b
 
-console.log(statusCode);
-
-
-//below we are seeing nuber values like 200,202 we are getting twice because of the reverse mapping
-//output
-// {
-//   '200': 'Success',
-//   '202': 'Accepted',
-//   NotFound: 'karun',
-//   Success: 200,
-//   Accepted: 202,
-//   BadRequest: '400'
-// }
